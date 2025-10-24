f243862e620f574e9edda9b1ec21ce7656b33dbb
JamieNudge <roastedtoe@googlemail.com>
2025-10-24 14:15:11 +0100

    Initial Commit

---
+++ Doomscroll Stopper/Doomscroll_StopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doomscroll_StopperApp.swift
+//  Doomscroll Stopper
+//
+//  Created by Jamie on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Doomscroll_StopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Doomscroll StopperTests/Doomscroll_StopperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Doomscroll_StopperTests.swift
+//  Doomscroll StopperTests
+//
+//  Created by Jamie on 24/10/2025.
+//
+
+import Testing
+@testable import Doomscroll_Stopper
+
+struct Doomscroll_StopperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
